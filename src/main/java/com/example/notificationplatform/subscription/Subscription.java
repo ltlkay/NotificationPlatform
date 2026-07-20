57c70ec3f404d3c18a08a8d7abf2d1c744c9d195
Denis C <denis_chupin@yahoo.ca>
2026-07-20 16:38:15 -0400

    NP-1 - null-checks and consistency

--- src/main/java/com/example/notificationplatform/subscription/Subscription.java
+++ src/main/java/com/example/notificationplatform/subscription/Subscription.java
@@ -4,15 +4,12 @@ import com.example.notificationplatform.util.ChannelType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
 import java.util.Objects;
 import java.util.UUID;
 
 @Entity
-@EntityListeners(AuditingEntityListener.class)
 @Table(name = "subscriptions", indexes = {
         @Index(name = "idx_event_type", columnList = "event_type"),
         @Index(name = "idx_channel_type", columnList = "channel")
@@ -24,23 +21,22 @@ public class Subscription {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "event_type")
+    @Column(name = "event_type", nullable = false)
     private String eventType;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "channel")
+    @Column(name = "channel", nullable = false)
     private ChannelType channel;
 
-    @Basic(optional = false)
+    @Column(nullable = false)
     private String target;
 
-    @CreatedDate
     @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private Instant createdAt;
 
     public Subscription(String eventType, ChannelType channel, String target){
-        this.eventType = eventType;
-        this.channel = channel;
+        this.eventType = Objects.requireNonNull(eventType);
+        this.channel = Objects.requireNonNull(channel);
         this.target = Objects.requireNonNull(target);
         this.createdAt = Instant.now();
     }
