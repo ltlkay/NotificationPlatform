f5f5adaea303154d0f7ed1017060318ba077e3f1
Denis C <denis_chupin@yahoo.ca>
2026-07-20 21:06:46 -0400

    NP-3 - Clean up

--- src/main/java/com/example/notificationplatform/subscription/SubscriptionRequest.java
+++ src/main/java/com/example/notificationplatform/subscription/SubscriptionRequest.java
@@ -4,8 +4,6 @@ import com.example.notificationplatform.util.ChannelType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import java.util.Objects;
-
 public record SubscriptionRequest(
         @NotBlank String eventType,
         @NotNull ChannelType channel,
